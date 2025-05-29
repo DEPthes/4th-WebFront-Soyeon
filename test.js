af3029d17f224f8de1b7ce984910dcf7e5d47ee7
soyeon <soyeon20031010@gmail.com>
2025-05-29 19:22:12 +0900

    feat: week7 정리본

--- test.js
+++ test.js
@@ -1,1 +1,24 @@
-globalThis === this
+function AnimalFunc(name) {
+  this.name = name;
+}
+
+class AnimalClass {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+const a1= AnimalFunc('초코');
+console.log(a1);//1
+
+const a2 = AClass();
+console.log(a2);//2
+
+class AClass{
+	say(){
+		console.log("hello");
+	}
+}
+
+const a3= AnimalClass ('뽀삐');
+console.log(a2);//3
